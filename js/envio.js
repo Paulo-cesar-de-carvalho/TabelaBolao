f6babfb6fb8c4c8f86f17233eb7df6f0a16cb51f
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-09-14 22:36:18 -0300

    3

--- js/envio.js
+++ js/envio.js
@@ -1,4 +1,25 @@
 let btnEnviar = document.querySelector("#btn-enviar")
 btnEnviar.addEventListener("click",function(){
     alert("Em contrução")
+    //criar valida dados
+    
+    extrair_dados()
+    //criar enviar dados
+    console.log(dadosCabecalho)
+    let teste = document.querySelector("#Sim-whatsapp")
+    console.log(teste)
+
 })
+
+let dadosCabecalho = []
+function extrair_dados(){
+    dadosCabecalho.push(document.querySelector("#nome-completo").value)
+    dadosCabecalho.push(document.querySelector("#cidade").value)
+    dadosCabecalho.push(document.querySelector("#celular").value)
+    dadosCabecalho.push(document.querySelector("#email").value)
+    dadosCabecalho.push(document.querySelector("#Sim-whatsapp").value)
+    dadosCabecalho.push(document.querySelector("#concordo").value)
+}
+
+//extrair_dados()
+//console.log(dadosCabecalho)
--- js/selecoes.js
+++ js/selecoes.js
@@ -1,3 +1,4 @@
+//Grupos da copa:
 let grupoA =['Catar','Equador','Senegal','Holanda']
 let grupoB =['Inglaterra','Irã','Estados Unidos','País de Gales']
 let grupoC =['Argentina','Arábia Saudita','México','Polônia']
@@ -6,42 +7,37 @@ let grupoE =['Espanha','Costa Rica','Alemanha','Japão']
 let grupoF =['Bélgica','Canadá','Marrocos','Croácia']
 let grupoG =['Brasil','Sérvia','Suíça','Camarões']
 let grupoH =['Portugal','Gana','Uruguai','Coreia do Sul']
-
 let selecoes = [grupoA,grupoB,grupoC,grupoD,grupoE,grupoF,grupoG,grupoH]
+
+//Ordem dos jogos:
 let ordem1 = [1,3,1,4,4,2]
 let ordem2 = [2,4,3,2,1,3]
 let grupoLetra = ["A","B","C","D","E","F","G","H"]
 
+//Ordem Alfabética:
+let selecoesAlfabetica = []
+for (time=0;time<4;time++){
+    for (grupos=0;grupos<8;grupos++){        
+        selecoesAlfabetica.push(selecoes[grupos][time])
+    }
+}
+selecoesAlfabetica.sort()
+
+//Carregar seleções nas opçoes em ordem alfabética:
+function carregar_opcoes(campo){
+    for (selecao in selecoesAlfabetica){
+        let opcao = document.createElement("option")
+        opcao.innerText = selecoesAlfabetica[selecao]
+        campo.appendChild(opcao)
+    }
+}
+
 let opcao_campeao = document.querySelector("#campeao")
 let opcao_vice = document.querySelector("#vice-campeao")
 let opcao_terceiro = document.querySelector("#terceiro")
+let opcao_quarto = document.querySelector("#quarto")
 
-//reduzir esses três próxmidos códigos e ordenar por ordem alfabética
-//criar uma função ordenando por odem alfabética
-
-for (time=0;time<4;time++){
-    for (grupos=0;grupos<8;grupos++){        
-        let selecao = selecoes[grupos][time]
-        let opcao = document.createElement("option")
-        opcao.innerText = selecao
-        opcao_campeao.appendChild(opcao)
-       
-    }    
-}
-for (time=0;time<4;time++){
-    for (grupos=0;grupos<8;grupos++){        
-        let selecao = selecoes[grupos][time]
-        let opcao = document.createElement("option")
-        opcao.innerText = selecao
-        opcao_vice.appendChild(opcao)
-    }    
-}
-
-for (time=0;time<4;time++){
-    for (grupos=0;grupos<8;grupos++){        
-        let selecao = selecoes[grupos][time]
-        let opcao = document.createElement("option")
-        opcao.innerText = selecao
-        opcao_terceiro.appendChild(opcao)
-    }    
-}
+carregar_opcoes(opcao_campeao)
+carregar_opcoes(opcao_vice)
+carregar_opcoes(opcao_terceiro)
+carregar_opcoes(opcao_quarto)
