e7c237c092aa9b9feaacd82df2d2666ced20939e
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-09-20 22:00:33 -0300

    8

--- js/envio.js
+++ js/envio.js
@@ -23,3 +23,52 @@ function extrair_dados(){
 
 //extrair_dados()
 //console.log(dadosCabecalho)
+/*function escreverArquivo() {  
+
+    var fso  = new ActiveXObject("Scripting.FileSystemObject");
+    var fh = fso.CreateTextFile("C:\Users\paulo\OneDrive\FUTEBOL\Bolão Copa 2022\tesste1.txt", true); 
+    fh.WriteLine("Coloque todo o conteudo que voce deseja nesse trecho...");
+    fh.Close(); 
+
+}
+escreverArquivo()*/
+
+//Cria Objeto ActiveX
+
+/*var dados = new ActiveXObject("Scripting.FileSystemObject")
+
+//Função para gravar o arquivo
+function GravaArquivo(arq , texto){
+    //pasta a ser salvo o arquivo
+    let pasta = "C:/Users/paulo/OneDrive/FUTEBOL/Bolão Copa 2022/"
+
+    if ( arq == "") {
+        arq= "Sem Titulo"
+    }
+    
+    //carrega o txt
+    var esc = dados.CreateTextFile( pasta + arq + ".txt", false)
+    //escreve o que foi passado no parametro texto que é o texto contido no TextArea
+    esc.WriteLine(texto)
+    //fecha o txt
+    esc.Close()
+}
+
+GravaArquivo("tesste1","Este é um teste sendo escrito")
+*/
+
+/*
+//Função para abrir o arquivo
+function AbreArquivo(arq){
+//o parametro arq é o endereço do txt
+//carrega o txt
+var arquivo = dados.OpenTextFile(arq, 1, true);
+//varre o arquivo
+while(!arquivo.AtEndOfStream){
+//escreve o txt no TextArea
+document.getElementById(“texto”).value = arquivo.ReadAll();
+}
+//fecha o txt
+arquivo.Close();
+}
+*/
