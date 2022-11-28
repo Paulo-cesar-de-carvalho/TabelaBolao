689c41dbaf135c55aeb0c7fc4a21020488106eda
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-11-27 21:54:29 -0300

    1

--- js/envio.js
+++ js/envio.js
@@ -1,30 +1,32 @@
 
-function hora_atual (){
+// function hora_atual (){
 
-    let data = new Date ()
-    let ano = (data.getYear()-100).toString()
-    let mes = (data.getMonth()+1).toString()
-    mes = ((mes.length==1)? `0${mes}`: mes)
-    let dia = data.getDate().toString()
-    dia = ((dia.length==1)? `0${dia}`: dia)
-    let hora = data.getHours().toString()
-    hora = ((hora.length==1)? `0${hora}`: hora)
-    let min = data.getMinutes().toString()
-    min = ((min.length==1)? `0${min}`: min)
-    let segundo = data.getSeconds().toString()
-    segundo = ((segundo.length==1)? `0${segundo}`: segundo)
-    return (`${ano}-${mes}-${dia} ${hora}-${min}-${segundo} `)
+//     let data = new Date ()
+//     let ano = (data.getYear()-100).toString()
+//     let mes = (data.getMonth()+1).toString()
+//     mes = ((mes.length==1)? `0${mes}`: mes)
+//     let dia = data.getDate().toString()
+//     dia = ((dia.length==1)? `0${dia}`: dia)
+//     let hora = data.getHours().toString()
+//     hora = ((hora.length==1)? `0${hora}`: hora)
+//     let min = data.getMinutes().toString()
+//     min = ((min.length==1)? `0${min}`: min)
+//     let segundo = data.getSeconds().toString()
+//     segundo = ((segundo.length==1)? `0${segundo}`: segundo)
+//     return (`${ano}-${mes}-${dia} ${hora}-${min}-${segundo} `)
 
-}
+// }
 
 let divHora = document.querySelector("#data")
 let btnEnviar = document.querySelector("#btn-enviar")
 
-divHora.value = hora_atual()
+//divHora.value = hora_atual()
 
 btnEnviar.addEventListener("click", function() {
-    divHora.value = hora_atual()
+    divHora.value = ((new Date()).toLocaleString())
 })
+
+console.log ((new Date()).toLocaleString())
 
 // function hora_atual_simplicifada(){
 //     let data = new Date ()
