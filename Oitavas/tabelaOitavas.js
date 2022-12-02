c31434fc8a5570c0c377f512addcbe1e32ca0437
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2022-12-02 15:21:15 -0300

    1

--- Oitavas/tabelaOitavas.js
+++ Oitavas/tabelaOitavas.js
@@ -89,7 +89,7 @@ let d1 = "França"
 let e1 = "Japão"
 let f1 = "Marrocos"
 let g1 = "Brasil"
-let h1 = "Uruguai"
+let h1 = "Portugal"
 let a2 = "Senegal"
 let b2 = "Estados Unidos"
 let c2 = "Polônia"
@@ -97,14 +97,14 @@ let d2 = "Austrália"
 let e2 = "Espanha"
 let f2 = "Croácia"
 let g2 = "Suíça"
-let h2 = "Portugal"
+let h2 = "Coreia do Sul"
 
 // ordem jogos:
 let selecoesMandantes = [a1, c1, d1, b1, e1, g1, f1, h1]
 let selecoesVisitante = [b2, d2, c2, a2, f2, h2, e2, g2]
 
 const dataHoraOitavas = ["03/12/22 sáb 12:00 - Estádio Inter. Khalifa, Al Rayyan","03/12/22 sáb 16:00 - Estádio Ahmed Bin Ali, Al Rayyan",
-    "04/12/22 dom 12:00 - Estádio Al Bayt, Al Khor","04/12/22 dom 16:00 - Estádio Al Thumama, Doha","05/12/22 seg 12:00 - Estádio Al Janoub, Al Wakrah",
+    "04/12/22 dom 12:00 - Estádio Al Thumama, Doha","04/12/22 dom 16:00 - Estádio Al Bayt, Al Khor","05/12/22 seg 12:00 - Estádio Al Janoub, Al Wakrah",
     "05/12/22 seg 16:00 - Estádio 974, Doha","06/12/22 ter 12:00 - Est. Cidade da Educação, Al Rayyan","06/12/22 ter 16:00 - Estádio Nacional Lusail"]
 
 criar_oitavas_de_final(selecoesMandantes,selecoesVisitante, dataHoraOitavas)
