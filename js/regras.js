8b7a8f3c4d0883017d6f3a4edbe8bbb340af2962
Paulo César de Carvalho <paulocesarcarvalho@rocketmail.com>
2026-07-01 22:29:08 -0300

    1
    1

--- js/regras.js
+++ js/regras.js
@@ -4,7 +4,7 @@ let btnCopiar = document.querySelector("#btn-copiar")
 
 btnCopiar.addEventListener("click", async function copiarTexto() {
    try {
-    await navigator.clipboard.writeText("00020126330014br.gov.bcb.pix011100582793696520400005303986540550.005802BR5923PAULO CESAR DE CARVALHO6012POUSO ALEGRE62150511Bolao16avos6304E1CD");
+    await navigator.clipboard.writeText("00020126330014br.gov.bcb.pix011100582793696520400005303986540540.005802BR5923PAULO CESAR DE CARVALHO6012POUSO ALEGRE62160512BolaoOitavas6304858B");
     alert("Código Pix Copiado com Sucesso");
   } catch (err) {
     console.error("Falha ao copiar: ", err);
